4ae1c0efcb85a9310998bab2fae5bc0d72269e1e
= <=>
2019-01-18 15:55:48 +0100

    Ergonomie améliorée

--- app/src/main/java/com/cpe/funconnect/funconnect/activities/DrawConnect.kt
+++ app/src/main/java/com/cpe/funconnect/funconnect/activities/DrawConnect.kt
@@ -2,9 +2,13 @@ package com.cpe.funconnect.funconnect.activities
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Toast
 import com.cpe.funconnect.funconnect.*
 import com.cpe.funconnect.funconnect.model.User
@@ -13,24 +17,49 @@ import com.cpe.funconnect.funconnect.task.ConnectTask
 import com.cpe.funconnect.funconnect.task.RegisterTask
 import com.cpe.funconnect.funconnect.task.RegisterTask.Companion.answer
 import kotlinx.android.synthetic.main.activity_draw.*
+import kotlinx.android.synthetic.main.activity_validation.*
 import org.json.JSONObject
 
 class DrawConnect: DrawActivity() {
 
     protected var connectTask: ConnectTask? = null
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 4000 //4 seconds
+    private var isChecked: Boolean = false
+    private lateinit var rocketAnimation: AnimatedVectorDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_draw)
         super.onCreate(savedInstanceState)
         attemptText.text = "Attempt : $attempt"
+        expandcollapse.apply {
+            rocketAnimation = background as AnimatedVectorDrawable
+        }
+    }
+
+    internal val mRunnable: Runnable = Runnable {
+        if (!isFinishing) {
+            finish()
+        }
     }
 
     override fun onLastReply(success : Boolean) {
         super.onLastReply(success)
         if (success){
-            val intent = Intent(this, ValidationScreenActivity::class.java)
+
+            //Initialize the Handler
+            mDelayHandler = Handler()
+            drawLayout.visibility = INVISIBLE
+            progressBar?.visibility = INVISIBLE
+            validation.visibility = VISIBLE
+
+            rocketAnimation.start()
+
+            //Navigate with delay
+            mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
+          /*  val intent = Intent(this, ValidationScreenActivity::class.java)
             startActivity(intent)
-            finish()
+            finish()*/
         }
         else{
             if(attempt+1 != 4 ){
--- app/src/main/java/com/cpe/funconnect/funconnect/activities/ValidationScreenActivity.kt
+++ app/src/main/java/com/cpe/funconnect/funconnect/activities/ValidationScreenActivity.kt
@@ -1,14 +1,23 @@
 package com.cpe.funconnect.funconnect.activities
 
+import android.graphics.drawable.AnimatedStateListDrawable
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import com.cpe.funconnect.funconnect.R
+import kotlinx.android.synthetic.main.activity_validation.*
+
+
+
 
 class ValidationScreenActivity: AppCompatActivity()  {
 
     private var mDelayHandler: Handler? = null
     private val SPLASH_DELAY: Long = 4000 //4 seconds
+    private var isChecked: Boolean = false
+    private lateinit var rocketAnimation: AnimatedVectorDrawable
+
 
     internal val mRunnable: Runnable = Runnable {
         if (!isFinishing) {
@@ -22,10 +31,19 @@ class ValidationScreenActivity: AppCompatActivity()  {
 
         //Initialize the Handler
         mDelayHandler = Handler()
+        expandcollapse.apply {
+            rocketAnimation = background as AnimatedVectorDrawable
+        }
 
         //Navigate with delay
         mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
     }
+
+    override fun onStart() {
+        super.onStart()
+        rocketAnimation.start()
+}
+
 
     public override fun onDestroy() {
 
@@ -36,4 +54,7 @@ class ValidationScreenActivity: AppCompatActivity()  {
         super.onDestroy()
     }
 
+
 }
+
+
